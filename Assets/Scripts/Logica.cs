172fb5e8a41aa1399767c13deeb4542f8e651116
pedroryansc <pedroryangamer@gmail.com>
2024-06-06 18:08:11 -0300

    Implementação completa da tela de Game Over

--- Assets/Scripts/Logica.cs
+++ Assets/Scripts/Logica.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Logica : MonoBehaviour
 {
-    
+
+    public AudioSource erroGameOver;
+
+    void Start(){
+        if(SceneManager.GetActiveScene().name == "GameOver"){
+            erroGameOver.Play();
+        }
+    }
+
     public void gameOver(){
         SceneManager.LoadScene("GameOver");
+    }
+
+    public void restartGame(){
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void voltarTelaTitulo(){
+        SceneManager.LoadScene("TelaTitulo");
     }
 
 }
