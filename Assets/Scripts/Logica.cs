715b8762e9acdf78cded5cab15a30cdb537cac18
yohanes <yzanghelini@gmail.com>
2024-06-14 07:52:20 -0300

    adding files

--- Assets/Scripts/Logica.cs
+++ Assets/Scripts/Logica.cs
@@ -11,13 +11,22 @@ public class Logica : MonoBehaviour
     public Image img;
     public Text mensagem;
     public float tempo = 0;
+    public static Text bestTimesText;
+
+    void Start() {
+        bestTimesText = GameObject.Find("BestTimeText").GetComponent<Text>();
+        DisplayBestTimes();
+    }
 
     void Update(){
+
         if(tempo < 7){
             tempo += Time.deltaTime;
-        } else{
+        } else if(tempo > 7) {
             mensagem.enabled = false;
         }
+        
+
     }
 
     public void playGame(){
@@ -44,7 +53,49 @@ public class Logica : MonoBehaviour
     }
 
     public void fim(){
+        float finishTime = Time.time - PlayerMovimento.startTime;
+        SaveBestTime(finishTime);
         SceneManager.LoadScene("FimLabirinto");
     }
+
+
+     public static void SaveBestTime(float newTime)
+{
+    float[] bestTimes = new float[4];
+    for (int i = 0; i < 3; i++)
+    {
+        bestTimes[i] = PlayerPrefs.GetFloat("BestTime" + (i + 1), float.MaxValue);
+    }
+    bestTimes[3] = newTime;
+
+    System.Array.Sort(bestTimes);
+
+    for (int i = 0; i < 3; i++)
+    {
+        PlayerPrefs.SetFloat("BestTime" + (i + 1), bestTimes[i]);
+    }
+
+    PlayerPrefs.Save();
+}
+
+public static void DisplayBestTimes()
+    {
+        string bestTimesTextContent = "";
+        for (int i = 0; i < 3; i++)
+        {
+            float bestTime = PlayerPrefs.GetFloat("BestTime" + (i + 1), float.MaxValue);
+            if (bestTime < float.MaxValue)
+            {
+                bestTimesTextContent += (i + 1) + ". " + bestTime.ToString("F2") + " seconds\n";
+            }
+            else
+            {
+                bestTimesTextContent += (i + 1) + ". N/A\n";
+            }
+        }
+
+        bestTimesText.text = bestTimesTextContent;
+    }
+
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,67 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovimento : MonoBehaviour
 {
-
-    Rigidbody2D rigidbody2d;
-    float vertical;
-    float horizontal;
+    private Rigidbody2D rigidbody2d;
+    private float vertical;
+    private float horizontal;
     public Logica logica;
+    public Animator animator;
+    public Vector2 teste;
+    public Text timeText;
+
+    public static float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        // QualitySettings.vSyncCount = 0;
-        // Application.targetFrameRate = 60;
         rigidbody2d = GetComponent<Rigidbody2D>();
         logica = GameObject.FindGameObjectWithTag("Logica").GetComponent<Logica>();
+        timeText = GameObject.Find("TimeText").GetComponent<Text>();
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        vertical = 0.0f;
-        horizontal = 0.0f;
         
-        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
-            vertical = 1.0f;
-        } else if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
-            vertical = -1.0f;
-        }
+            vertical = 0.0f;
+            horizontal = 0.0f;
 
-        // Debug.Log(vertical);
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                vertical = 1.0f;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                vertical = -1.0f;
+            }
 
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
-            horizontal = -1.0f;
-        } else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
-            horizontal = 1.0f;
-        }
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                horizontal = -1.0f;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                horizontal = 1.0f;
+            }
 
-        // Debug.Log(horizontal);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                logica.som.Play();
+                StartCoroutine(logica.flashMonitor());
+                logica.img.color = new Color(0, 0, 0, 0);
+            }
 
-        if(Input.GetKey(KeyCode.Space)){
-            logica.som.Play();
-            StartCoroutine(logica.flashMonitor());
-            logica.img.color = new Color(0, 0, 0, 0);
-        }
+            teste = new Vector2(horizontal, vertical);
+
+            animator.SetFloat("vertical", vertical);
+            animator.SetFloat("horizontal", horizontal);
+            animator.SetFloat("velocity", teste.sqrMagnitude);
+
+            if (teste != Vector2.zero)
+            {
+                animator.SetFloat("horizontalIdle", horizontal);
+                animator.SetFloat("verticalIdle", vertical);
+            }
+
+            float currentTime = Time.time - startTime;
+            timeText.text = "Timer: " + currentTime.ToString("F2") + " seconds";
+        
     }
 
-    void FixedUpdate(){
-        Vector2 position = rigidbody2d.position;
+    void FixedUpdate()
+    {
+        
+            Vector2 position = rigidbody2d.position;
 
-        position.y = position.y + 3.7f * vertical * Time.deltaTime;
-        position.x = position.x + 3.7f * horizontal * Time.deltaTime;
+            position.y += 3.7f * vertical * Time.deltaTime;
+            position.x += 3.7f * horizontal * Time.deltaTime;
 
-        rigidbody2d.MovePosition(position);
+            rigidbody2d.MovePosition(position);
+        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "Virus"){
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Virus")
+        {
             logica.gameOver();
-        } else if(collision.gameObject.tag == "Fim do Labirinto"){
+        }
+        else if (collision.gameObject.tag == "Fim do Labirinto")
+        {
             logica.fim();
         }
     }
+
+
+
+
 }
